5508785cf80bf9d9d44faa25bf797080db62b3ce
mim321 <45837613+mim321@users.noreply.github.com>
2020-09-14 13:12:50 +0700

    Add files via upload

---
+++ joom/joom.php
@@ -0,0 +1,159 @@
+
+
+<!DOCTYPE html><html lang="th"><head><title>Mim เว็บเพจ (Mim webpage)</title>
+<meta charset="utf-8" /><meta name="viewport" content="width=device-width,initial-scale=1" />
+<meta name="keywords" content="joom,onepagedesign,responsive" /><meta name="description" content="จ๋อมเว็บเพจ คือ เว็บเพจตัวอย่างที่เป็นผลจากการออกแบบเว็บเพจแบบสแตติกโดยใช้ inline css และ internal css ให้รองรับการแสดงผลกับอุปกรณ์ที่หลากหลายได้เหมาะสม (responsive) ปรับเว็บเพจให้ส่งเข้าประเมินจาก page speed แล้วได้คะแนนสูง และอัพโหลดไปยังฟรีเว็บโฮสติ้งต่าง ๆ แล้วสามารถเรียกใช้โดยไม่ต้องปรับแต่งเพิ่มเติม" />
+<meta property="og:image" content="http://www.thaiall.com/actress/nok_mypictureonmagazine.jpg" />
+<link type="text/css" rel="stylesheet" href="https://thaiwebsearch.hypermart.net/actress/rsp80.css" /><link rel="icon" type="image/x-icon" href="https://thaiwebsearch.hypermart.net/actress/rsp.ico" />
+<style>body{background-color:#F5F5DC !important;}li{float:left;width:120px;background-color:#FFB6C1;margin:5px;border:1px outset gray;border-radius:3px;}p{float:left;}
+.blockcenter{text-align:center; margin:auto;} .blockhidden {text-align:center;margin:auto;} .imgborder {border:1px outset white;border-radius:20px;box-shadow:5px 5px 5px gray; width : 100%;} 
+.covercard{width:1100px;font-size:20px;color:white;margin-top:1px;display:inline-block;}
+.cardleft{width:20%;float:left;}.cardright{width:80%;float:left;}div.menuleft{text-align:left;margin:auto;margin-top:2px;width:90%;border:1px solid gray;border-radius:5px;}
+.card1{float:left;width:99px;height:140px;background-color:Aquamarine;border-radius:5px;padding:5px;margin:5px;}.card2{float:left;width:175px;height:170px;background-color:blue;border-radius:3px;padding:3px;margin:3px;}
+.card_article_70{float:left;width:745px;text-align:left;}.card_article_30{float:left;width:345px;text-align:left;}
+@media only screen and (min-width:376px) and (max-width:768px){body{background-color:#000000 !important;}.covercard{width:95%;}.card1{width:85px;height:100px;background-color:#000000;}.card2{width:175px;height:auto;}.cardleft{width:99%;}.cardright{background-color:white !important;width:99%;}.imgborder{width:75%;}.card_article_70{float:left;width:99%;text-align:left;}.card_article_30{float:left;width:99%;text-align:left;}}
+@media only screen and (max-width:375px){body{background-color:#000000 !important;}.covercard{width:99%;}li{width:100px;font-size:16px;}.card1{width:97%;height:auto;}.card2{width:170px;height:auto;float:left;background-color:#000000;}.cardleft{width:99%;font-size:16px;}.cardright{background-color:#000000 !important;width:99%;}.blockhidden{visibility:hidden;display:none;}.imgborder{width:60%;}}
+@font-face{font-family:'WRTishkid2';src:url('rsp_wr_tish_kid2.ttf');font-display:swap;}</style>
+<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/css/lightbox.css" type="text/css" media="screen" />
+</head><body style="font-family:wrtishkid2;">
+
+<br/>ปรับปรุง : 2563-07-29 (ส่งเข้า firebase)
+<!-- มิ้ม (Mim) -->
+<!-- 8.0 -->
+
+<!-- Part : 1 = cover photo -->
+<div class="blockcenter">
+<div class="covercard" style="background-color:#000000;color:white;">
+<img src="https://raw.githubusercontent.com/mim321/joom/master/77.jpg" alt="77" style="width:100%;float:left;display:inline-block;" />
+</div>
+</div>
+
+<!-- Part : 2 = menu bar -->
+<div class="blockcenter">
+<div class="covercard" style="background-color:#FA8072;color:blue;">
+<ul style="list-style-type:none;padding:0px;margin:0px;">
+<li><a href="https://web.facebook.com/mim.suchicha" target="_blank">FB Profile</a></li>
+<li><a href="https://raw.githubusercontent.com/mim321/joom/master/55.jpg" title="Tifa ff7" target="_blank">Retouch</a></li>
+<li><a href="file:///C:/Users/mim%20jaa/Desktop/joom/joom.htm" target="_blank">Joom100</a></li>
+</ul>
+</div>
+</div>
+
+<!-- Part : 3 = cardleft กับ cardright -->
+<div class="blockcenter">
+<div class="covercard" style="background-color:white;">
+<div class="cardleft">
+<div style="width:100%;background-color:white;display:inline-block;text-align:left;color:black;">
+<img src="https://raw.githubusercontent.com/mim321/joom/master/555.jpeg" alt="joom_cam" style="float: left;height: 90px;border: 1px solid #ddd;border-radius: 5px;padding: 5px;" />
+<b>Mim เว็บเพจ</b> .. ภาพเชตนี้ดิฉันได้มาจากทางอินเทอร์เน็ต เป็นภาพของสัตร์ต่าง ๆ ที่เห็นเเล้วเกิดความรู้สึกประทับใจ ก็เลยนำภาพเซตนี้มานำเสนอผ่านทางเว็บเพจนี้
+</div>
+
+<div style="width:100%;background-color:white;display:inline-block;"><!-- menu block -->
+<div class="menuleft" style="background-color:#FF1493;color:white;text-align:center;width:95%;">
+การพัฒนาเว็บเพจ
+</div>
+<div class="menuleft" style="background-color:#FFF0F5;">
+<span class="glyphicon glyphicon-home" style="color:black;font-size:12px;"></span> <a href="file:///C:/Users/mim%20jaa/Desktop/joom/joom.htm">Home</a>
+</div>
+<div class="menuleft" style="background-color:#FAEBD7;">
+<span class="glyphicon glyphicon-home" style="color:black;font-size:12px;"></span> <a href="https://imnichapa.wordpress.com/?fbclid=IwAR2utSJChY_Y0p4VqktB-w0AjH5iSrjsYFXIaa8E-e72ziz_ic4gKxnK0H8">wordpress</a>
+</div>
+<div class="menuleft" style="background-color:#FFF0F5;">
+<span class="glyphicon glyphicon-home" style="color:black;font-size:12px;"></span> <a href="https://www.youtube.com/channel/UCHqdeiHTJPLjayV1gW-2Y7A?view_as=subscriber">youtube</a>
+</div>
+<div class="menuleft" style="background-color:#FAEBD7;">
+<span class="glyphicon glyphicon-home" style="color:black;font-size:12px;"></span> <a href="https://github.com/mim321">github</a>
+</div>
+<div class="menuleft" style="background-color:#FFF0F5;">
+<span class="glyphicon glyphicon-home" style="color:black;font-size:12px;"></span> <a href="https://nichapagm.blogspot.com/2020/08/google-meet-video-conferencing-platform.html">blogspot</a>
+</div>
+
+
+<div class="menuleft" style="background-color:#FF1493;color:white;text-align:center;width:95%;">
+About me
+</div>
+<div class="menuleft" style="background-color:#FFE4E1;">
+<span class="glyphicon glyphicon-home" style="color:black;font-size:12px;"></span> <a href="https://web.facebook.com/mim.suchicha">facebook</a>
+<a href="file:///C:/Users/mim%20jaa/Desktop/joom/joom.htm">#</a>
+</div>
+<div class="menuleft" style="background-color:#FFFAFA;">
+<span class="glyphicon glyphicon-home" style="color:black;font-size:12px;"></span> <a href="https://raw.githubusercontent.com/mim321/joom/master/333.jpg">line</a>
+<a href="file:///C:/Users/mim%20jaa/Desktop/joom/joom.htm">#</a>
+</div>
+<div class="menuleft" style="background-color:#FFE4E1;">
+<span class="glyphicon glyphicon-home" style="color:black;font-size:12px;"></span> <a href="https://www.instagram.com/">instagram</a>
+</div>
+<div class="menuleft" style="background-color:#FFFAFA;">
+<span class="glyphicon glyphicon-home" style="color:black;font-size:12px;"></span> <a href="https://twitter.com/home">twitter</a>
+</div>
+
+<div class="menuleft" style="background-color:white;text-align:center;border:0px;">
+<span class="glyphicon glyphicon-heart" style="color:red;"></span>
+</div>
+</div><!-- menu block - inline -->
+</div><!-- cardleft -->
+<div class="cardright" style="background-color:#00CED1;border:1px solid white;border-radius:5px;">
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/1.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/1.jpg" alt="43" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/2.1.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/2.1.jpg" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/3.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/3.jpg" alt="45" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/4.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/4.jpg" alt="46" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/5.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/5.jpg" alt="47" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/6.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/6.jpg" alt="48" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/7.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/7.jpg" alt="37" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/8.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/8.jpg" alt="38" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/9.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/9.jpg" alt="49" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/10.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/10.jpg" alt="40" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/11.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/11.jpg" alt="41" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/12.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/12.jpg" alt="41" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/13.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/13.jpg" alt="31" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/14.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/14.jpg" alt="32" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/15.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/15.jpg" alt="33" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/16.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/16.jpg" alt="34" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/17.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/17.jpg" alt="35" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/18.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/18.jpg" alt="36" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/19.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/19.jpg" alt="25" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/20.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/20.jpg" alt="26" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/21.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/21.jpg" alt="27" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/2.2.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/2.2.jpg" alt="28" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/23.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/23.jpg" alt="29" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/24.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/24.jpg" alt="30" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/25.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/25.jpg" alt="19" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/26.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/26.jpg" alt="20" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/27.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/27.jpg" alt="21" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/28.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/28.jpg" alt="22" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/29.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/29.jpg" alt="23" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/30.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/30.jpg" alt="24" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/31.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/31.jpg" alt="13" class="imgborder" /></a></div>
+<div class="card1"><a href="https://raw.githubusercontent.com/mim321/joom/master/32.jpg" rel="lightbox[roadtrip]"><img src="https://raw.githubusercontent.com/mim321/joom/master/32.jpg" alt="14" class="imgborder" /></a></div>
+</div><!-- cardright -->
+</div><!-- covercard -->
+</div><!-- blockcenter -->
+
+<!-- Part : 4 = retouch album -->
+
+
+<!-- Part : 5 = conclusion -->
+<div class="blockhidden">
+<div class="covercard" style="color:black;">
+<div class="card_article_30">
+
+
+<iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2FBumble-MIM-109172497597415%3Fref%3Dsgm%23%21%2FBumble-MIM-109172497597415%3Fref%3Dsgm&width=400&connections=9&stream=true&header=true&height=550" 
+scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:300px; height:550px;" allowTransparency="true"></iframe>
+</td><td  style="background-color:white;" id="c5">
+</div>
+</div><!-- blockhidden -->
+
+<div class="blockcenter" style="color:black;text-align:center;">
+<script>
+<!--
+document.write("<div style='text-align:center;'>Height = " + document.body.clientHeight + " Width = " + document.body.clientWidth + "</div>")
+url=document.location.href.substr(7,11)
+page="actress_joom"
+if(url == "www.thaiall") document.write("<script language='javascript1.1' src='http://hits1.truehits.net/data/c0001941.js'></script>")
+-->
+</script>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.10.0/js/lightbox-plus-jquery.js"></script>
+</body></html>
